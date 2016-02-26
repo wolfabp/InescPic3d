35d9ce1772d26ec5c2f4932b6ae35601e2236be7
wolfabp <abpintor@hotmail.com>
2016-02-26 17:19:14 +0000

    Revert "clean code on getTime.m"
    This reverts commit 1bf7fba89737264d9feb39620ebc5845819620fe.

--- getTime.m
+++ getTime.m
@@ -1,12 +1,15 @@
 function [result] = getTime(str)
-    if(strfind(str,'Patient')>0)
-        k = strfind(str, '_');
-        h = str2double(str((k(6)+1): (k(6)+2)));
-        m = str2double(str((k(7)+1): (k(7)+2)));
-        s = str2double(str((k(8)+1): (k(8)+2)));
-        ms =str2double(str((k(9)+1): (k(9)+3)));
-        result =(h*60*60*1000)+(m*60*1000)+(s*1000)+ms;
-    else
-        result=-1;
-    end
+% str
+if(strfind(str,'Patient')>0)
+    %str
+    k = strfind(str, '_');
+    h = str2double(str((k(6)+1): (k(6)+2)));
+    m = str2double(str((k(7)+1): (k(7)+2)));
+    s = str2double(str((k(8)+1): (k(8)+2)));
+    ms =str2double(str((k(9)+1): (k(9)+3)));
+    result =(h*60*60*1000)+(m*60*1000)+(s*1000)+ms;
+    %pause()
+else
+    result=-1;
+end
 end
